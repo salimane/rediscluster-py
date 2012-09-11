93202d90bf13e26a480d03fe10364bdf1927152a
Salimane Adjao Moustapha <me@salimane.com>
2012-09-11 15:00:11 +0800

    update pypi build script

--- pypi-build.sh
+++ pypi-build.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
+. /home/salimane/htdocs/env/rediscluster2.7/bin/activate
 python setup.py sdist upload
-python2.7 setup.py bdist_egg upload --quiet
-python2.7 setup.py bdist_wininst --target-version=2.7 --plat-name=win32 register upload --quiet
-. /home/salimane/htdocs/env/rediscluster3/bin/activate
-python3.2 setup.py bdist_egg upload --quiet
-python3.2 setup.py bdist_wininst --target-version=3.2 --plat-name=win32 register upload --quiet
+python setup.py bdist_egg upload --quiet
+python setup.py bdist_wininst --target-version=2.7 --plat-name=win32 register upload --quiet
+deactivate
+. /home/salimane/htdocs/env/rediscluster3.2/bin/activate
+python setup.py bdist_egg upload --quiet
+python setup.py bdist_wininst --target-version=3.2 --plat-name=win32 register upload --quiet
 deactivate

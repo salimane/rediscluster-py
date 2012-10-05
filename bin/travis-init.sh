709a6c7e5d14f30d020012d3556f2dba6a5f7572
Salimane Adjao Moustapha <me@salimane.com>
2012-10-05 22:44:33 +0800

    setup cluster in travis build env

---
+++ bin/travis-init.sh
@@ -0,0 +1,4 @@
+sh -c "/usr/bin/redis-server /home/travis/builds/salimane/rediscluster-py/bin/redis/redis-node-1.conf"
+sh -c "/usr/bin/redis-server /home/travis/builds/salimane/rediscluster-py/bin/redis/redis-node-2.conf"
+sh -c "/usr/bin/redis-server /home/travis/builds/salimane/rediscluster-py/bin/redis/redis-node-5.conf"
+sh -c "/usr/bin/redis-server /home/travis/builds/salimane/rediscluster-py/bin/redis/redis-node-6.conf"

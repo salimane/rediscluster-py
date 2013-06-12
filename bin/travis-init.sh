791602c5e063927e6b43e7c32bf831dfebabba30
Salimane Adjao Moustapha <me@salimane.com>
2013-06-12 18:15:35 +0800

    support drone.io ci by removing hardcoded redis-server path

--- bin/travis-init.sh
+++ bin/travis-init.sh
@@ -1,4 +1,4 @@
-sh -c "/usr/bin/redis-server $CI_HOME/bin/redis/redis-node-1.conf --dir ${CI_HOME}/bin/redis --include ${CI_HOME}/bin/redis/redis-common.conf"
-sh -c "/usr/bin/redis-server $CI_HOME/bin/redis/redis-node-2.conf --dir ${CI_HOME}/bin/redis --include ${CI_HOME}/bin/redis/redis-common.conf"
-sh -c "/usr/bin/redis-server $CI_HOME/bin/redis/redis-node-5.conf --dir ${CI_HOME}/bin/redis --include ${CI_HOME}/bin/redis/redis-common.conf"
-sh -c "/usr/bin/redis-server $CI_HOME/bin/redis/redis-node-6.conf --dir ${CI_HOME}/bin/redis --include ${CI_HOME}/bin/redis/redis-common.conf"
+sh -c "`which redis-server` $CI_HOME/bin/redis/redis-node-1.conf --dir ${CI_HOME}/bin/redis --include ${CI_HOME}/bin/redis/redis-common.conf"
+sh -c "`which redis-server` $CI_HOME/bin/redis/redis-node-2.conf --dir ${CI_HOME}/bin/redis --include ${CI_HOME}/bin/redis/redis-common.conf"
+sh -c "`which redis-server` $CI_HOME/bin/redis/redis-node-5.conf --dir ${CI_HOME}/bin/redis --include ${CI_HOME}/bin/redis/redis-common.conf"
+sh -c "`which redis-server` $CI_HOME/bin/redis/redis-node-6.conf --dir ${CI_HOME}/bin/redis --include ${CI_HOME}/bin/redis/redis-common.conf"
